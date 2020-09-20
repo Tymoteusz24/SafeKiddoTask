ea6bc67045b87d04db644bf279d925b50933944c
Tymoteusz Pasieka <tymoteusz.pasieka@gmail.com>
2020-09-20 19:19:50 +0200

    Initial Commit

---
+++ SafeKiddoRecruitmentTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafeKiddoRecruitmentTask
+//
+//  Created by Tymoteusz Pasieka on 20/09/2020.
+//  Copyright © 2020 Tymoteusz Pasieka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
