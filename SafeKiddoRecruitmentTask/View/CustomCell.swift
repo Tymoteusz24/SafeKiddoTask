e5a306d639795c1f6677a93bfd6a53fd6e644b69
Tymoteusz Pasieka <tymoteusz.pasieka@gmail.com>
2020-09-21 10:01:59 +0200

    task completed

---
+++ SafeKiddoRecruitmentTask/View/CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  SafeKiddoRecruitmentTask
+//
+//  Created by Tymoteusz Pasieka on 20/09/2020.
+//  Copyright © 2020 Tymoteusz Pasieka. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SafeKiddoRecruitmentTask/ViewModel/CellViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CellViewModel.swift
+//  SafeKiddoRecruitmentTask
+//
+//  Created by Tymoteusz Pasieka on 20/09/2020.
+//  Copyright © 2020 Tymoteusz Pasieka. All rights reserved.
+//
+
+import Foundation
